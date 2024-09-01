949973474dae925ded2ef3d7237f7601622703cc
allstarcom <sasialex21@gmail.com>
2024-09-01 16:33:19 -0400

    Additional CRUD operation for animals

--- backend/controllers/animal.controller.js
+++ backend/controllers/animal.controller.js
@@ -9,5 +9,49 @@ const createAnimal = async (req, res) => {
     }
 };
 
+const editAminal = async (req, res) => {
+    try {
+        const animal = await Animal.findOneAndUpdate(req.params.id, req.body);
+        res.json(animal);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const editAllAminals = async (req, res) => {
+    try {
+        const animals = await Animal.updateMany({}, req.body);
+        res.json(animals);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const deleteAnimal = async (req, res) => {
+    try {
+        const animal = await Animal.findByIdAndDelete(req.params.id);
+        res.json(animal);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }    
+};
+
+const getAllAnimals = async (req, res) => {
+    try {
+        const animals = await Animal.find();
+        res.json(animals);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const getSpecificAnimal = async (req, res) => {
+    try {
+        const animal = await Animal.findById(req.params.id);
+        res.json(animal);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
 
 module.exports = { createAnimal };
